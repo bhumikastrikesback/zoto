678043c98ee61c33b316088830a76c3d6d8ace8e
Bhumika Arora <bhumika0077@pop-os.localdomain>
2025-08-23 19:19:59 +0530

    Added Restaurant, MenuItem and RestrauntManager header file

---
+++ managers/RestrauntManager.h
@@ -0,0 +1,45 @@
+#ifndef RESTAURANT_MANAGER_H
+#define RESTAURANT_MANAGER_H
+
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "../models/Restaurant.h"
+using namespace std;
+
+class RestaurantManager {
+    private:
+        vector<Restaurant*> restraunts;
+        static RestaurantManager* instance;
+        
+        RestaurantManager() {
+            //private construstor
+        }
+    public:
+        static RestaurantManager* getinstance(){
+            if(!instance) {
+                instance = new RestaurantManager();
+            }
+            return instance;
+        }
+        void addRestraunt(Restaurant* r) {
+            restraunts.push_back(r);
+        }
+        vector<Restaurant*> seacrchByLocation(string loc){
+            vector<Restaurant*> result;
+            transform(loc.begin(), loc.end(), loc.begin(), :: tolower);
+            for(auto r : restraunts) {
+                string rl = r->getLocation();
+                transform(rl.begin(), rl.end(), rl.begin(), :: tolower);
+                if(rl == loc) {
+                    result.push_back(r);
+                }
+            }
+
+            return result;
+        }
+};
+
+RestaurantManager* RestaurantManager::instance = nullptr;
+
+#endif
